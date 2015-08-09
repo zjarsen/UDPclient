b8818f95039502e70ae73ebf35a4ef01a883ca11
Andy Jiahui Z <zjarsen@gmail.com>
2015-08-09 12:00:30 -0700

    milestone

--- Source.cpp
+++ Source.cpp
@@ -1,28 +1,18 @@
-#define PLATFORM_WINDOWS  1
-
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define PLATFORM PLATFORM_WINDOWS
-#include <winsock2.h>
+#include <WinSock2.h>
+#include <Windows.h>
 #pragma comment(lib,"ws2_32.lib")
 #define PORTNUM 12354
 #define BUFMAX 1024
 
-void windowsEchoLoop(SOCKET, SOCKADDR*, size_t);
-int windowsEchoClient();
-
 int main(int argc, char** argv) {
-	windowsEchoClient();
+	//try the system start
+	//system("start C:\\Users\\zack\\Desktop\\eyeTracking-offscreenCalibration\\Debug\\eyeTracking-offscreenCalibration.exe");
 
-	puts("Press any key to continue");
-	getc(stdin);
-
-	return EXIT_SUCCESS;
-}
-
-int windowsEchoClient()
-{
+	//add
 	SOCKET sock;
 	sockaddr_in serverAddr;
 	WSADATA wsaDat;
@@ -43,42 +33,53 @@ int windowsEchoClient()
 
 		serverAddr.sin_family = AF_INET;
 		serverAddr.sin_port = htons(PORTNUM);
-		serverAddr.sin_addr.s_addr = inet_addr("10.42.0.1");
+		serverAddr.sin_addr.s_addr = inet_addr("172.22.254.14");
 
 		sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-		windowsEchoLoop(sock, (SOCKADDR*)& serverAddr, sizeof(serverAddr));
+		//windowsEchoLoop(sock, (SOCKADDR*)& serverAddr, sizeof(serverAddr));
+
+		//add windowsEchoLoop
+		int bytesRead;
+		int sendToResult;
+		char inputBuffer[BUFMAX] = { 0 };
+		char recvBuffer[BUFMAX] = { 0 };
+
+		//AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+		//automatically start the eye tracking program when this program starts
+		sendto(sock, "udp client started", strlen("udp client started"), 0, (SOCKADDR*)& serverAddr, sizeof(serverAddr));
+		//system("start C:\\Users\\zack\\Desktop\\eyeTracking-offscreenCalibration\\Debug\\eyeTracking-offscreenCalibration.exe");
+		
+
+		for (;;)
+		{
+			printf("Type message: ");
+			fgets(inputBuffer, BUFMAX, stdin);
+
+			sendToResult = sendto(sock, inputBuffer, strlen(inputBuffer), 0, (SOCKADDR*)& serverAddr, sizeof(serverAddr));
+
+			if (sendToResult == SOCKET_ERROR) {
+				wprintf(L"sendto failed with error: %d\n", WSAGetLastError());
+				closesocket(sock);
+				WSACleanup();
+
+				puts("Press any key to continue");
+				getc(stdin);
+				exit(sendToResult);
+			}
+
+
+		}
+		//end
 	}
 	else
 	{
 		return EXIT_FAILURE;
 	}
-}
+	//end
 
-void windowsEchoLoop(SOCKET sock, SOCKADDR* serverAddr, size_t serverlen)
-{
-	int bytesRead;
-	int sendToResult;
-	char inputBuffer[BUFMAX] = { 0 };
-	char recvBuffer[BUFMAX] = { 0 };
+	puts("Press any key to continue");
+	getc(stdin);
 
-	for (;;)
-	{
-		printf("Type message: ");
-		fgets(inputBuffer, BUFMAX, stdin);
-
-		sendToResult = sendto(sock, inputBuffer, strlen(inputBuffer), 0, serverAddr, serverlen);
-
-		if (sendToResult == SOCKET_ERROR) {
-			wprintf(L"sendto failed with error: %d\n", WSAGetLastError());
-			closesocket(sock);
-			WSACleanup();
-
-			puts("Press any key to continue");
-			getc(stdin);
-			exit(sendToResult);
-		}
-
-		
-	}
+	return EXIT_SUCCESS;
 }
